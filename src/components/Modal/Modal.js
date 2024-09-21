af63ac8ef029ad5832c6f9de9cab7c849aa11915
eid <eidhachem1@gmail.com>
2024-09-21 13:05:08 +0300

    Add transition from react-tansition-group

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,7 +3,14 @@ import React from "react"
 import "./Modal.css"
 
 const modal = (props) => {
-  const cssClasses = ["modal", props.show ? "modal-open" : "modal-close"]
+  const cssClasses = [
+    "modal",
+    props.show === "entering"
+      ? "modal-open"
+      : props.show === "exiting"
+      ? "modal-close"
+      : null,
+  ]
   return (
     <div className={cssClasses.join(" ")}>
       <h1>A Modal</h1>
